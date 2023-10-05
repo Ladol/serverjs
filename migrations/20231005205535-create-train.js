607d4010d3eaac2dda742226975c7b87b364a711
Ladol <ladol2003@gmail.com>
2023-10-05 22:01:55 +0100

    changed config, migrations and Train to train.js

---
+++ migrations/20231005205535-create-train.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Trains', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      tableName: {
+        type: Sequelize.STRING
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      departureTime: {
+        type: Sequelize.TIME
+      },
+      arrivalTime: {
+        type: Sequelize.TIME
+      },
+      stationsData: {
+        type: Sequelize.JSONB
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Trains');
+  }
+};
--- models/Train.js
+++ models/Train.js
@@ -1,36 +1,27 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('yxaysfby', 'yxaysfby', '2t27aEF1m7eYM95PD8CpVOb937ZenlAf', {
-    host: 'cornelius.db.elephantsql.com', // Replace with your ElephantSQL database's hostname
-    port: 5432, // Default PostgreSQL port
-    dialect: 'postgres',
-    dialectModule: require('pg'),
-});
-
-const Train = sequelize.define('Train', {
-    // Dynamic table name based on number and date
-    tableName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-    },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    departureTime: {
-        type: DataTypes.TIME,
-        allowNull: false,
-    },
-    arrivalTime: {
-        type: DataTypes.TIME,
-        allowNull: false,
-    },
-    // JSON data for stations, pass status, scheduled time, and observations
-    stationsData: {
-        type: DataTypes.JSONB,
-    },
-}, {
-    // Other options and configurations for your model
-});
-
-module.exports = Train;
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Train extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Train.init({
+    tableName: DataTypes.STRING,
+    name: DataTypes.STRING,
+    departureTime: DataTypes.TIME,
+    arrivalTime: DataTypes.TIME,
+    stationsData: DataTypes.JSONB
+  }, {
+    sequelize,
+    modelName: 'Train',
+  });
+  return Train;
+};
